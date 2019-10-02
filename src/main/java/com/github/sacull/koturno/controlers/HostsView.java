2de79bb92ec7ef8349f5f1d991fe71e0644feeee
Łukasz Dębicki <sacull@gmail.com>
2019-10-02 20:54:01 +0200

    Add history to view

--- src/main/java/com/github/sacull/koturno/controlers/HostsView.java
+++ src/main/java/com/github/sacull/koturno/controlers/HostsView.java
@@ -1,6 +1,7 @@
 package com.github.sacull.koturno.controlers;
 
 import com.github.sacull.koturno.entities.Host;
+import com.github.sacull.koturno.entities.Inaccessibility;
 import com.github.sacull.koturno.utils.LifeChecker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +23,8 @@ public class HostsView {
     @RequestMapping("/")
     public String viewIndex() {
         StringBuilder result = new StringBuilder("<html><h1> >< Koturno ><</h1>");
-        TypedQuery<Host> query = em.createQuery("SELECT h FROM Host h", Host.class);
-        List<Host> hosts = query.getResultList();
+        TypedQuery<Host> hostQuery = em.createQuery("SELECT h FROM Host h", Host.class);
+        List<Host> hosts = hostQuery.getResultList();
         if (hosts.size() == 0) {
             result.append("No hosts in database!<br>");
         } else {
@@ -35,6 +36,22 @@ public class HostsView {
                 } else {
                     result.append(" is OFFLINE<br>");
                 }
+            }
+        }
+        result.append("<h2>History</h2>");
+        TypedQuery<Inaccessibility> inaccessibilityQuery =
+                em.createQuery("SELECT i FROM Inaccessibility i", Inaccessibility.class);
+        List<Inaccessibility> inaccessibilities = inaccessibilityQuery.getResultList();
+        if (inaccessibilities.size() == 0) {
+            result.append("No history in database<br>");
+        } else {
+            for (Inaccessibility inaccessibility : inaccessibilities) {
+                result.append(inaccessibility.getHost().getDestination());
+                result.append(" was OFFLINE from ");
+                result.append(inaccessibility.getStart());
+                result.append(" to ");
+                result.append(inaccessibility.getEnd());
+                result.append("<br>");
             }
         }
         result.append("</html>");
