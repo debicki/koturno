06f6c622d445905b69bbc5b940b45ab226b53dae
Łukasz Dębicki <sacull@gmail.com>
2019-11-08 20:16:29 +0100

    Refactor all repositories

--- src/main/java/com/github/sacull/koturno/repositories/HostRepository.java
+++ src/main/java/com/github/sacull/koturno/repositories/HostRepository.java
@@ -1,41 +1,7 @@
 package com.github.sacull.koturno.repositories;
 
 import com.github.sacull.koturno.entities.Host;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
-import java.util.List;
-
-@Repository
-@Transactional
-public class HostRepository {
-
-    @Autowired
-    private EntityManager em;
-
-    public Host getById(Long id) {
-        return em.find(Host.class, id);
-    }
-
-    public void deleteById(Long id) {
-        Host hostToDelete = getById(id);
-        em.remove(hostToDelete);
-    }
-
-    public Host save(Host host) {
-        if (host.getId() == null) {
-            em.persist(host);
-        } else {
-            em.merge(host);
-        }
-        return host;
-    }
-
-    public List<Host> getAllHosts() {
-        TypedQuery<Host> getAllHostsQuery = em.createQuery("SELECT h FROM Host h", Host.class);
-        return getAllHostsQuery.getResultList();
-    }
+public interface HostRepository extends JpaRepository<Host, Long> {
 }
--- src/main/java/com/github/sacull/koturno/repositories/InaccessibilityRepository.java
+++ src/main/java/com/github/sacull/koturno/repositories/InaccessibilityRepository.java
@@ -1,61 +1,7 @@
 package com.github.sacull.koturno.repositories;
 
-import com.github.sacull.koturno.entities.Host;
 import com.github.sacull.koturno.entities.Inaccessibility;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
-import java.util.List;
-
-@Repository
-@Transactional
-public class InaccessibilityRepository {
-
-    @Autowired
-    private EntityManager em;
-
-    public Inaccessibility getById(Long id) {
-        return em.find(Inaccessibility.class, id);
-    }
-
-    public void deleteById(Long id) {
-        Inaccessibility inaccessibilityToDelete = getById(id);
-        em.remove(inaccessibilityToDelete);
-    }
-
-    public Inaccessibility save(Inaccessibility inaccessibility) {
-        if (inaccessibility.getId() == null) {
-            em.persist(inaccessibility);
-        } else {
-            em.merge(inaccessibility);
-        }
-        return inaccessibility;
-    }
-
-    public List<Inaccessibility> getAllInaccessibilities() {
-        TypedQuery<Inaccessibility> getAllInaccessibilitiesQuery =
-                em.createQuery("SELECT i FROM Inaccessibility i", Inaccessibility.class);
-        return getAllInaccessibilitiesQuery.getResultList();
-    }
-
-    public Inaccessibility getLastInaccessibility(Host host) {
-        return this.getById(
-                host.getInaccessibilities()
-                        .get(host.getInaccessibilities().size() - 1)
-                        .getId());
-    }
-
-    public Inaccessibility getLastHostInaccessibility(Host host) {
-        if (host.getInaccessibilities().size() > 0) {
-            return this.getById(
-                    host.getInaccessibilities()
-                            .get(host.getInaccessibilities().size() - 1)
-                            .getId());
-        } else {
-            return null;
-        }
-    }
+public interface InaccessibilityRepository extends JpaRepository<Inaccessibility, Long> {
 }
