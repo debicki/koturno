b40a72ff48294bdd86be9be49253cac953010a8a
Łukasz Dębicki <sacull@gmail.com>
2020-01-21 14:29:10 +0100

    Transfer import logic (#29)
    * Create service and move support methods
    * Move import logic from controller to service
    * Remove unused elements

---
+++ src/main/java/com/github/sacull/koturno/services/FileService.java
@@ -0,0 +1,129 @@
+package com.github.sacull.koturno.services;
+
+import com.github.sacull.koturno.entities.HGroup;
+import com.github.sacull.koturno.entities.Host;
+import com.github.sacull.koturno.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class FileService {
+
+    private HostService hostService;
+    private HGroupService hGroupService;
+
+    @Autowired
+    public FileService(HostService hostService, HGroupService hGroupService) {
+        this.hostService = hostService;
+        this.hGroupService = hGroupService;
+    }
+
+    public Map<String, Integer> hostsImport(User loggedUser,
+                                            Map<String, Integer> report,
+                                            MultipartFile file) throws IOException {
+
+        int importSuccess = 0;
+        int importWarnings = 0;
+        int importErrors = 0;
+
+        List<Host> importList = new ArrayList<>();
+        BufferedReader fileContent = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        String line;
+        while ((line = fileContent.readLine()) != null) {
+            if (!line.trim().startsWith("#") && !line.trim().startsWith("//") && !(line.trim().length() < 1)) {
+                Host hostToAdd = parse(line);
+                hostToAdd.setOwner(loggedUser);
+                importList.add(hostToAdd);
+            }
+        }
+
+        importErrors = importList.size();
+        List<Host> hostsInDatabase = hostService.getAllHostsByUser(loggedUser);
+        for (Host host : hostsInDatabase) {
+            importList = importList.stream().filter(x -> !x.compareAddress(host)).collect(Collectors.toList());
+        }
+        importErrors -= importList.size();
+
+        HGroup defaultGroup = hGroupService.getGroupByName("default");
+        for (Host host : importList) {
+            if (host.getName().equals("") || host.getName() == null) {
+                host.setHostGroup(defaultGroup);
+            } else {
+                HGroup group = hGroupService.getGroupByName(host.getName());
+                if (group == null) {
+                    group = new HGroup(host.getName(), "");
+                    group = hGroupService.save(group);
+                }
+                host.setHostGroup(group);
+            }
+            host.setOwner(loggedUser);
+            if (isValidAddress(host.getAddress())) {
+                importSuccess++;
+            } else {
+                importWarnings++;
+            }
+        }
+
+        if (importList.size() > 0) {
+            for (Host host : importList) {
+                hostService.save(host);
+            }
+        }
+
+        report.replace("importSuccess", importSuccess);
+        report.replace("importWarnings", importWarnings);
+        report.replace("importErrors", importErrors);
+
+        return report;
+    }
+
+    public boolean isValidAddress(String address) {
+        InetAddress host;
+        try {
+            host = InetAddress.getByName(address);
+        } catch (UnknownHostException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private Host parse(String line) {
+        int charCounter = 0;
+        line = line.replace('\t', ' ');
+        StringBuilder address = new StringBuilder();
+        StringBuilder name = new StringBuilder();
+        StringBuilder description = new StringBuilder();
+        while (line.charAt(charCounter) == ' ') {
+            charCounter++;
+        }
+        while ((charCounter < line.length()) && (line.charAt(charCounter) != ' ')) {
+            address.append(line.charAt(charCounter));
+            charCounter++;
+        }
+        while ((charCounter < line.length()) && (line.charAt(charCounter) == ' ')) {
+            charCounter++;
+        }
+        String[] descriptionElements = line.substring(charCounter).split("\\*");
+        if (descriptionElements.length > 1) {
+            name.append(descriptionElements[0]);
+            for (int i = 1; i < descriptionElements.length; i++) {
+                description.append(descriptionElements[i]);
+                description.append(' ');
+            }
+        } else if (descriptionElements.length == 1) {
+            description.append(descriptionElements[0]);
+        }
+        return new Host(name.toString(), address.toString(), description.toString(), null, null);
+    }
+}
