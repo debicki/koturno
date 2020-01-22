22785455bf216d3b3bc1877015f62350d4202b55
Łukasz Dębicki <sacull@gmail.com>
2020-01-22 14:18:52 +0100

    Add user password encrypt feature

--- src/main/java/com/github/sacull/koturno/services/UserService.java
+++ src/main/java/com/github/sacull/koturno/services/UserService.java
@@ -3,20 +3,23 @@ package com.github.sacull.koturno.services;
 import com.github.sacull.koturno.entities.User;
 import com.github.sacull.koturno.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private UserRepo userRepo;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepo userRepo) {
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder) {
         this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void registerUser(String username, String password, boolean active, String role) {
-        userRepo.save(new User(username, "{noop}" + password, active, role));
+        userRepo.save(new User(username, passwordEncoder.encode(password), active, role));
     }
 
     public User findByName(String name) {
