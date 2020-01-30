909442cbeb819bcbc7d83ddf1f67a5f95a0fb2f1
Łukasz Dębicki <sacull@gmail.com>
2020-01-30 18:37:49 +0100

    Add more tests for GroupPageController

--- src/test/java/com/github/sacull/koturno/controllers/GroupPageControllerTests.java
+++ src/test/java/com/github/sacull/koturno/controllers/GroupPageControllerTests.java
@@ -3,13 +3,16 @@ package com.github.sacull.koturno.controllers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.github.sacull.koturno.entities.*;
 import com.github.sacull.koturno.services.HGroupService;
 import com.github.sacull.koturno.services.HostService;
 import com.github.sacull.koturno.services.InaccessibilityService;
 import com.github.sacull.koturno.services.UserService;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +23,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.regex.Matcher;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,16 +38,16 @@ public class GroupPageControllerTests {
     private WebApplicationContext context;
 
     @MockBean
-    private HGroupService hGroupService;
+    private HGroupService hGroupServiceMock;
 
     @MockBean
-    private HostService hostService;
+    private HostService hostServiceMock;
 
     @MockBean
-    private InaccessibilityService inaccessibilityService;
+    private InaccessibilityService inaccessibilityServiceMock;
 
     @MockBean
-    private UserService userService;
+    private UserService userServiceMock;
 
     private MockMvc mvc;
 
@@ -59,7 +68,113 @@ public class GroupPageControllerTests {
 
     @Test
     @WithMockUser
-    public void shouldReturnValidModelAttributes() {
+    public void shouldReturnValidModelAttributesWithEmptyGroup() throws Exception {
+        User user = new User("user", "user", true, "ROLE_USER");
+        HGroup hGroup = new HGroup("group", "");
 
+        Mockito.when(hGroupServiceMock.getGroupById(Mockito.anyLong())).thenReturn(hGroup);
+        Mockito.when(userServiceMock.findByName(Mockito.anyString())).thenReturn(user);
+        Mockito.when(hostServiceMock.findAllByHostGroup(Mockito.any(HGroup.class), Mockito.any(User.class)))
+                .thenReturn(Collections.emptyList());
+        Mockito.when(inaccessibilityServiceMock.findAllByActiveIsTrue()).thenReturn(Collections.emptyList());
+
+        mvc.perform(get("/group?id=666"))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/views/group.jsp"))
+                .andExpect(model().attribute("group", Matchers.equalTo(hGroup)))
+                .andExpect(model().attribute("hosts", Matchers.hasSize(0)))
+                .andExpect(model().attribute("unstableHosts", Matchers.hasSize(0)))
+                .andExpect(model().attribute("offlineHosts", Matchers.hasSize(0)));
+
+        Mockito.verify(hGroupServiceMock, Mockito.times(1)).getGroupById(Mockito.anyLong());
+        Mockito.verify(userServiceMock, Mockito.times(1)).findByName(Mockito.anyString());
+        Mockito.verify(hostServiceMock, Mockito.times(1))
+                .findAllByHostGroup(Mockito.any(HGroup.class), Mockito.any(User.class));
+        Mockito.verify(inaccessibilityServiceMock, Mockito.times(1)).findAllByActiveIsTrue();
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnValidModelAttributesWithGroupWithTwoHosts() throws Exception {
+        User user = new User("user", "user", true, "ROLE_USER");
+        HGroup hGroup = new HGroup("group", "");
+        Host firstHost = new Host("firstHost", "localhost", "", hGroup, user);
+        Host secondHost = new Host("secondHost", "localhost", "", hGroup, user);
+
+        Mockito.when(hGroupServiceMock.getGroupById(Mockito.anyLong())).thenReturn(hGroup);
+        Mockito.when(userServiceMock.findByName(Mockito.anyString())).thenReturn(user);
+        Mockito.when(hostServiceMock.findAllByHostGroup(Mockito.any(HGroup.class), Mockito.any(User.class)))
+                .thenReturn(Arrays.asList(firstHost, secondHost));
+        Mockito.when(inaccessibilityServiceMock.findAllByActiveIsTrue()).thenReturn(Collections.emptyList());
+
+        mvc.perform(get("/group?id=666"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("group", Matchers.equalTo(hGroup)))
+                .andExpect(model().attribute("hosts", Matchers.hasSize(2)))
+                .andExpect(model().attribute("hosts", Matchers.hasItem(
+                        Matchers.hasProperty("name", Matchers.is("firstHost")))))
+                .andExpect(model().attribute("hosts", Matchers.hasItem(
+                        Matchers.hasProperty("name", Matchers.is("secondHost")))))
+                .andExpect(model().attribute("unstableHosts", Matchers.hasSize(0)))
+                .andExpect(model().attribute("offlineHosts", Matchers.hasSize(0)));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnValidModelAttributesWithGroupWithTwoUnstableAndOneOfflineHosts() throws Exception {
+        User user = new User("user", "user", true, "ROLE_USER");
+        HGroup hGroup = new HGroup("group", "");
+        Host firstHost = new Host("firstHost", "localhost", "", hGroup, user);
+        Host secondHost = new Host("secondHost", "localhost", "", hGroup, user);
+        Host thirdHost = new Host("thirdHost", "localhost", "", hGroup, user);
+        Host fourthHost = new Host("fourthHost", "localhost", "", hGroup, user);
+        IGroup iGroup = new IGroup("group", "");
+        Inaccessibility firstInaccessibility = new Inaccessibility(firstHost, "firstInaccessibility", iGroup);
+        Inaccessibility secondInaccessibility = new Inaccessibility(thirdHost, "secondInaccessibility", iGroup);
+        Inaccessibility thirdInaccessibility = new Inaccessibility(fourthHost, "thirdInaccessibility", iGroup);
+        secondInaccessibility.setOfflineStatus(true);
+
+        Mockito.when(hGroupServiceMock.getGroupById(Mockito.anyLong())).thenReturn(hGroup);
+        Mockito.when(userServiceMock.findByName(Mockito.anyString())).thenReturn(user);
+        Mockito.when(hostServiceMock.findAllByHostGroup(Mockito.any(HGroup.class), Mockito.any(User.class)))
+                .thenReturn(Arrays.asList(firstHost, secondHost, thirdHost, fourthHost));
+        Mockito.when(inaccessibilityServiceMock.findAllByActiveIsTrue())
+                .thenReturn(Arrays.asList(firstInaccessibility, secondInaccessibility, thirdInaccessibility));
+
+        mvc.perform(get("/group?id=666"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("group", Matchers.equalTo(hGroup)))
+                .andExpect(model().attribute("hosts", Matchers.hasSize(4)))
+                .andExpect(model().attribute("unstableHosts", Matchers.hasSize(2)))
+                .andExpect(model().attribute("offlineHosts", Matchers.hasSize(1)))
+                .andExpect(model().attribute("offlineHosts", Matchers.hasItem(
+                        Matchers.hasProperty("name", Matchers.is("thirdHost")))));
+    }
+
+    @Test(expected = NestedServletException.class)
+    @WithMockUser
+    public void shouldReturnErrorWhenInvalidGroupIdRequested() throws Exception {
+        Mockito.when(hGroupServiceMock.getGroupById(Mockito.anyLong())).thenThrow(new EntityNotFoundException());
+
+        mvc.perform(get("/group?id=666"));
+
+        Mockito.verify(hGroupServiceMock, Mockito.times(1)).getGroupById(Mockito.anyLong());
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldSuccessfulDeleteGroup() throws Exception {
+        User user = new User("user", "user", true, "ROLE_USER");
+        HGroup hGroup = new HGroup("group", "");
+
+        Mockito.when(hGroupServiceMock.getGroupById(Mockito.anyLong())).thenReturn(hGroup);
+        Mockito.when(userServiceMock.findByName(Mockito.anyString())).thenReturn(user);
+        Mockito.when(hostServiceMock.findAllByHostGroup(Mockito.any(HGroup.class), Mockito.any(User.class)))
+                .thenReturn(Collections.emptyList());
+        Mockito.when(hostServiceMock.countAllByHostGroup(hGroup)).thenReturn(0L);
+
+        mvc.perform(get("/group?id=666&action=remove"))
+                .andExpect(flash().attribute("error", Matchers.is("10")))
+                .andExpect(status().is3xxRedirection());
     }
 }
